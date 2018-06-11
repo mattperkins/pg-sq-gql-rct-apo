7384d434ad45686c53c00e10809e4d2515f07170
mattperkins <github@mattperkins.com>
2018-06-11 11:33:23 +0100

    seed database with test data

--- server/db.js
+++ server/db.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize'
+import _ from 'lodash'
+import Faker from 'faker'
 
 const Connection = new Sequelize(
     'relay',
@@ -45,3 +47,23 @@ const Post = Connection.define('post', {
 Person.hasMany(Post)
 Post.hasMany(Person)
 
+
+// Open connection to db and install/sync tables
+
+// Each time App runs : reinitialise tables
+Connection.sync({force: true}).then(()=> {
+
+    // add fixtures using the "lodash utility belt" 
+    // add 10 new users at a time
+    _.times(10, ()=> {
+        return Person.create({
+            // seed the db with test data (Faker)
+            firstName: Faker.name.firstName(),
+            lastName: Faker.name.lastName(),
+            email: Faker.internet.email(),
+        })
+    })
+
+})
+
+export default Connection
