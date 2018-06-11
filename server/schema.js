d055c90a4d8467c6c4b6f5edd65d80127a9d0e70
mattperkins <github@mattperkins.com>
2018-06-11 13:48:09 +0100

    add graphql schema query

---
+++ server/schema.js
@@ -0,0 +1,91 @@
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLSchema} = require('graphql')
+const Db = require('.db')
+
+const Person = new GraphQLObjectType({
+    name: 'Person',
+    description: 'This represents a Person',
+    fields: ()=>{
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(person){
+                    return person.id
+                }
+            },
+            firstName: {
+                type: GraphQLString,
+                resolve(person){
+                    return person.firstName
+                }
+            },
+            lastName: {
+                type: GraphQLString,
+                resolve(person){
+                    return person.lastName
+                }
+            },
+            email: {
+                type: GraphQLString,
+                resolve(person){
+                    return person.email
+                }
+            }
+        }
+    }
+})
+
+const Post = new GraphQLObjectType({
+     name: 'Post',
+     description: 'This is a Post',
+     fields: ()=>{
+         return{
+            id: {
+                type: GraphQLInt,
+                resolve(person){
+                    return person.id
+                }
+            },
+            title: {
+                type: GraphQLString,
+                resolve(post){
+                    return post.title
+                }
+            },
+            content: {
+                type: GraphQLString,
+                resolve(post){
+                    return post.content
+                }
+            }           
+         }
+     }
+})
+
+const Query = new GraphQLObjectType({
+    name: 'Query',
+    description: 'This is a root query',
+    fields: ()=>{
+        return{
+            people:{
+                type: new GraphQLList(Person),
+                args:{
+                    id:{
+                        type: GraphQLInt
+                    },
+                    email:{
+                        type: GraphQLString
+                    }
+                },
+                resolve(root, args) {
+                    return Db.models.person.findAll({where: args})
+                }
+            }
+        }
+    }
+})
+
+const Schema = new GraphQLSchema({
+    query: Query
+})
+
+module.exports = Schema
