3de163d0e54f14261abeaa1a4316304d0df5c38e
mattperkins <github@mattperkins.com>
2018-06-11 10:34:06 +0100

    add table and relationships

--- server/db.js
+++ server/db.js
@@ -9,3 +9,39 @@ const Connection = new Sequelize(
         host: 'localhost'
     }
 )
+
+// Define Postgres tables
+const Person = Connection.define('person', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        // Sequelize built in validator
+        validate:{
+            isEmail: true
+        }
+    },
+})
+
+const Post = Connection.define('post', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+})
+
+// Define table relationships
+Person.hasMany(Post)
+Post.hasMany(Person)
+
